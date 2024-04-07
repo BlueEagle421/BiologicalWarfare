7092cb2653002e6efa98ee7e7395179238914b40
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-07 19:50:39 +0200

    Create CompBacteriaIncubator.cs

---
+++ Source/BiologicalWarfare/CompBacteriaIncubator.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiologicalWarfare
+{
+    public class CompProperties_VirusReplicator : CompProperties_Activable
+    {
+        public int maxRoomCellSize;
+        public CompProperties_VirusReplicator() => compClass = typeof(CompVirusReplicator);
+    }
+
+    public class CompBacteriaIncubator : CompActivable
+    {
+        public CompProperties_VirusReplicator ReplicatorProps => (CompProperties_VirusReplicator)props;
+
+        protected override bool TryUse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
