15452afaeed5f6a11d7c508ba1afc47ad97be492
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-07 14:38:15 +0200

    Update utils

--- Source/BiologicalWarfare/BiologicalUtils.cs
+++ Source/BiologicalWarfare/BiologicalUtils.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System.Collections.Generic;
 using Verse;
 
 namespace BiologicalWarfare
@@ -7,5 +8,32 @@ namespace BiologicalWarfare
     public static class BiologicalUtils
     {
         public static string ToStringUncapitalized(this DiseaseType diseaseType) => diseaseType.ToString().UncapitalizeFirst();
+
+        public static void SpawnThingAt(Map map, List<IntVec3> cells, ThingDef thingDef, int count)
+        {
+            for (int index = 0; index < cells.Count; ++index)
+            {
+                IntVec3 cell = cells[index];
+                if (cell.Walkable(map))
+                {
+                    Thing firstThing = cell.GetFirstThing(map, thingDef);
+                    if (firstThing != null)
+                    {
+                        if (firstThing.stackCount + count <= firstThing.def.stackLimit)
+                        {
+                            firstThing.stackCount += count;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Thing thing = ThingMaker.MakeThing(thingDef);
+                        thing.stackCount = count;
+                        if (GenPlace.TryPlaceThing(thing, cell, map, ThingPlaceMode.Near))
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
