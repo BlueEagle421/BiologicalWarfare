f4984c5258184e3f551d269f6b0c678cc945865b
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-15 21:13:59 +0200

    Improve VaccineResearchStation.cs

--- Source/BiologicalWarfare/VaccineResearchStation.cs
+++ Source/BiologicalWarfare/VaccineResearchStation.cs
@@ -1,35 +1,93 @@
-﻿using Verse;
+﻿using RimWorld;
+using Verse;
 
 namespace BiologicalWarfare
 {
-    public class CompVaccineResearchContainer : CompDiseaseSampleContainer
-    {
-        public override void PostSpawnSetup(bool respawningAfterLoad)
-        {
-            base.PostSpawnSetup(respawningAfterLoad);
-        }
-
-        public override AcceptanceReport CanExtract(Pawn pawn)
-        {
-            AcceptanceReport baseResult = base.CanExtract(pawn);
-
-            if (!baseResult.Accepted)
-                return baseResult;
-
-            return baseResult;
-        }
-    }
-
     public class BuildingVaccineResearchStation : Building
     {
-        public override string GetInspectString()
+        private CompDiseaseSampleContainer _compDiseaseContainer;
+        private CompPowerTrader _compPowerTrader;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
-            return "Researching...";
+            base.SpawnSetup(map, respawningAfterLoad);
+            _compDiseaseContainer = GetComp<CompDiseaseSampleContainer>();
+            _compPowerTrader = GetComp<CompPowerTrader>();
+        }
+        public ResearchProjectDef CurrentVaccineProject()
+        {
+            if (_compDiseaseContainer.Empty)
+                return null;
+
+            return _compDiseaseContainer.ContainedSampleComp().PropsDiseaseSample.combatDiseaseDef.vaccineResProjectDef;
         }
 
-        public void Test()
+        public bool CanPerformResearch(Pawn pawn)
         {
-            Log.Message("I'm working!");
+            if (CurrentVaccineProject() == null)
+                return false;
+
+            if (pawn == null)
+                return false;
+
+            if (pawn.WorkTypeIsDisabled(WorkTypeDefOf.Research))
+                return false;
+
+            if (!_compPowerTrader.PowerOn)
+                return false;
+
+            return true;
+        }
+
+        public override string GetInspectString()
+        {
+            string baseString = base.GetInspectString();
+
+            CompDiseaseSample diseaseSample = _compDiseaseContainer.ContainedSampleComp();
+
+            ResearchProjectDef vaccineProj = CurrentVaccineProject();
+
+            if (diseaseSample == null)
+                return baseString + string.Format("\n{0}", "USH_InsertSampleToResearch".Translate());
+
+            if (vaccineProj == null)
+                return baseString + string.Format("\n{0}", "USH_WrongSampleToResearch".Translate());
+
+            baseString += string.Format("\n{0}: {1}\n{2}: {3:F0} / {4:F0} ({5})", new object[]
+                {
+                    "CurrentProject".Translate(),
+                    vaccineProj.LabelCap,
+                    "ResearchProgress".Translate(),
+                    vaccineProj.ProgressApparent,
+                    vaccineProj.CostApparent,
+                    vaccineProj.ProgressPercent.ToStringPercent("0.#")
+                });
+
+            return baseString;
+        }
+
+        public void ResearchPerformed(float progress, Pawn researcher)
+        {
+            if (CurrentVaccineProject() == null)
+            {
+                Log.Error("Researched without having a vaccine project.");
+                return;
+            }
+
+            ResearchProjectDef vaccineProj = CurrentVaccineProject();
+
+            progress *= ResearchManager.ResearchPointsPerWorkTick;
+            progress *= Find.Storyteller.difficulty.researchSpeedFactor;
+
+            if (researcher?.Faction != null)
+                progress /= vaccineProj.CostFactor(researcher.Faction.def.techLevel);
+
+            if (DebugSettings.fastResearch)
+                progress *= 500f;
+
+            researcher?.records.AddTo(RecordDefOf.ResearchPointsResearched, progress);
+
+            Find.ResearchManager.AddProgress(vaccineProj, progress, researcher);
         }
     }
 }
