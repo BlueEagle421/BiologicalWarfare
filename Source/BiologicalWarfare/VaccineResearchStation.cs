60fe27961847d6322b5a6311244b9a88f218c8f3
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-15 12:15:11 +0200

    Create VaccineResearchStation.cs

---
+++ Source/BiologicalWarfare/VaccineResearchStation.cs
@@ -0,0 +1,35 @@
+﻿using Verse;
+
+namespace BiologicalWarfare
+{
+    public class CompVaccineResearchContainer : CompDiseaseSampleContainer
+    {
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+        }
+
+        public override AcceptanceReport CanExtract(Pawn pawn)
+        {
+            AcceptanceReport baseResult = base.CanExtract(pawn);
+
+            if (!baseResult.Accepted)
+                return baseResult;
+
+            return baseResult;
+        }
+    }
+
+    public class BuildingVaccineResearchStation : Building
+    {
+        public override string GetInspectString()
+        {
+            return "Researching...";
+        }
+
+        public void Test()
+        {
+            Log.Message("I'm working!");
+        }
+    }
+}
