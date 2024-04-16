16273d6ffeb5c60063e81c3d69f64bf922bccc8e
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-16 20:10:28 +0200

    Create ContaminationDoor.cs

---
+++ Source/BiologicalWarfare/ContaminationDoor.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BiologicalWarfare
+{
+    public class Building_ContaminationDoor : Building_Door
+    {
+        private CompPowerTrader _compPowerTrader;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            _compPowerTrader = GetComp<CompPowerTrader>();
+        }
+
+        public override bool PawnCanOpen(Pawn p)
+        {
+            if (!p.IsPlayerControlled)
+                base.PawnCanOpen(p);
+
+            if (_compPowerTrader.PowerOn && BiologicalUtils.CanPathogenInfect(p))
+                return false;
+
+            return base.PawnCanOpen(p);
+        }
+    }
+}
