e0d68ee4c640a710a7f5c11f5f258d55b1d13d2b
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-16 21:25:07 +0200

    Add filthDef

--- Source/BiologicalWarfare/CombatDiseaseDef.cs
+++ Source/BiologicalWarfare/CombatDiseaseDef.cs
@@ -18,6 +18,7 @@ namespace BiologicalWarfare
 
         public ThingDef sampleDef;
         public ThingDef pathogenDef;
+        public ThingDef filthDef;
 
         public ResearchProjectDef vaccineResProjectDef;
 
