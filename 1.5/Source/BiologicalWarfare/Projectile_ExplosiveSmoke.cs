bc8f5475dfdaba7eb92396142b9e7a0a4765ba7b
BlueEagle421 <joachim.wielewski@gmail.com>
2024-04-26 12:29:25 +0200

    Improve missiles

--- 1.5/Source/BiologicalWarfare/Projectile_ExplosiveSmoke.cs
+++ 1.5/Source/BiologicalWarfare/Projectile_ExplosiveSmoke.cs
@@ -9,6 +9,7 @@ namespace BiologicalWarfare
         private int _ticksUntilSmoke;
         private int _smokeSpawned;
         private static readonly IntRange SMOKE_INTERVAL = new IntRange(1, 25);
+        private static readonly int FIRE_CYCLE = 2;
         public override void Tick()
         {
             base.Tick();
@@ -18,12 +19,20 @@ namespace BiologicalWarfare
                 SpawnSmokeParticles();
         }
 
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref _ticksUntilSmoke, "USH_TicksUntilSmoke", 0);
+            Scribe_Values.Look(ref _smokeSpawned, "USH_SmokeSpawned", 0);
+        }
+
         private float ArcHeightFactor
         {
             get
             {
                 float arcHeightFactor = def.projectile.arcHeightFactor;
                 float magnitude = (destination - origin).MagnitudeHorizontalSquared();
+
                 if (arcHeightFactor * arcHeightFactor > magnitude * 0.2f * 0.2f)
                     arcHeightFactor = Mathf.Sqrt(magnitude) * 0.2f;
 
@@ -31,18 +40,23 @@ namespace BiologicalWarfare
             }
         }
 
+        private Vector3 WorldPosition()
+        {
+            float heightMultiplier = ArcHeightFactor * GenMath.InverseParabola(DistanceCoveredFractionArc);
+            return DrawPos + new Vector3(0f, 0f, 1f) * heightMultiplier;
+        }
+
         private void SpawnSmokeParticles()
         {
-            float num = ArcHeightFactor * GenMath.InverseParabola(DistanceCoveredFractionArc);
-            Vector3 vector = DrawPos + new Vector3(0f, 0f, 1f) * num;
+            Vector3 realPosition = WorldPosition();
 
-            FleckMaker.ThrowSmoke(vector, Map, 1f);
+            FleckMaker.ThrowSmoke(realPosition, Map, 1f);
 
-            if (_smokeSpawned % 2 == 0)
-                FleckMaker.ThrowFireGlow(vector, Map, 1f);
+            if (_smokeSpawned % FIRE_CYCLE == 0)
+                FleckMaker.ThrowFireGlow(realPosition, Map, 1f);
 
             _smokeSpawned++;
-            _ticksUntilSmoke = SMOKE_INTERVAL.Lerped(UnityEngine.Random.Range(0f, 1f));
+            _ticksUntilSmoke = SMOKE_INTERVAL.Lerped(Random.Range(0f, 1f));
         }
     }
 }
