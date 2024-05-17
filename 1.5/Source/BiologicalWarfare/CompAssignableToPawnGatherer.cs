89a6faa5f6b39c6303bc85afb4626c615d78b761
BlueEagle421 <joachim.wielewski@gmail.com>
2024-05-17 17:19:09 +0200

    Add mechanites gatherer

---
+++ 1.5/Source/BiologicalWarfare/CompAssignableToPawnGatherer.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace BiologicalWarfare
+{
+    public class CompAssignableToPawnGatherer : CompAssignableToPawn
+    {
+        public override string CompInspectStringExtra()
+        {
+            if (AssignedPawnsForReading.Count == 0)
+                return "USH_User".Translate() + ": " + "Nobody".Translate();
+
+            if (AssignedPawnsForReading.Count == 1)
+                return "USH_User".Translate() + ": " + AssignedPawnsForReading[0].Label;
+
+            return "USH_Users".Translate() + ": " + string.Join(", ", AssignedPawnsForReading.Select(x => x.LabelShortCap));
+        }
+
+        protected override string GetAssignmentGizmoLabel() => "USH_SetUsers".Translate();
+
+    }
+}
